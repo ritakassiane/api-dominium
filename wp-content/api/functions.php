df45af4fa1067e219938f1b7a051aa1203ee669f
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-07-26 08:38:33 -0300

    ADD: Configurando JWT

--- wp-content/api/functions.php
+++ wp-content/api/functions.php
@@ -1,12 +1,17 @@
-<?php 
+<?php
 
-// Registrando custom post types
 $template_diretorio = get_template_directory();
 
-require_once($template_diretorio . "/custom-post-type/direcao.php");
-require_once($template_diretorio . "/custom-post-type/administracao.php");
-require_once($template_diretorio . "/custom-post-type/funcionarios.php");
-require_once($template_diretorio . "/custom-post-type/professores-i.php");
-require_once($template_diretorio . "/custom-post-type/professores-ii.php");
+require_once($template_diretorio . "/custom-post-type/produto.php");
+require_once($template_diretorio . "/custom-post-type/transacao.php");
+
+require_once($template_diretorio . "/endpoints/usuario_post.php");
+require_once($template_diretorio . "/endpoints/usuario_get.php");
+
+// Token expira em 24h
+function expire_token() {
+  return time() + (60 * 60 * 24);
+}
+add_action('jwt_auth_expire', 'expire_token');
 
 ?>
