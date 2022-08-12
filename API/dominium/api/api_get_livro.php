98abef4cd12668316dc34265cda33d8df6d204fa
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-08-12 10:34:51 -0300

    WIP - EDIT: Puxando atributos exatos de livros

--- API/dominium/api/api_get_livro.php
+++ API/dominium/api/api_get_livro.php
@@ -2,9 +2,18 @@
 
 function api_get_livro ($request) {
     $slug = sanitize_text_field($request->get_param('slug'));
+    $page_object = get_page_by_path($slug, OBJECT, 'livros');
+
+    $id = $page_object->ID;
+    $title = $page_object->post_title;
+    $acf = get_fields($id);
+
 
     $livro = array(
         'slug' => $slug,
+        'id' => $id,
+        'title' => $title,
+        'acf' => $acf,
     );
 
     return rest_ensure_response( $livro );
