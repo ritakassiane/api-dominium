99ead0eacfc5c26a45f7a6cf9f76d30a28fa9e32
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-09-14 00:56:17 -0300

    BUILD: Reestruturando

---
+++ frontend/assets/js/index.js
@@ -0,0 +1,15 @@
+// Swiper dos cards dos blogs
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
