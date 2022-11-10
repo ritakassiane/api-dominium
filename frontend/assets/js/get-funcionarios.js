281f8044a76cdb2dd5b799c5f7bf4c20a6a58e5a
Rita Kassiane <ritakassiane.t.i@gmail.com>
2022-11-10 00:49:22 -0300

    Refact: Adicionando campo acf para administradores

--- frontend/assets/js/get-funcionarios.js
+++ frontend/assets/js/get-funcionarios.js
@@ -1,19 +1,39 @@
-let html_add_adm = '';
 let html_add_prof_i = '';
 let html_add_prof_ii = '';
 let html_add_func = '';
 
-fetch('https://dominium-api.nordevtech.com.br/wp-json/dominium/equipe/administracao')
-.then(
-    (responde) => console.log(responde.json()
+
+function get_funcionarios(tipo_de_funcionario, api_link){
+    let auxiliar = '';
+    fetch(api_link)
     .then(
-         data => {
-            let direcao = data[0]
-            console.log(direcao['0'])
-            direcao.forEach(element => {
-                console.log('aqui')
-                console.log(element)
-            })
-         }
-        ))
-)
+        (responde) => console.log(responde.json()
+        .then(
+            data => {
+                
+                console.log(data)
+                for (var key in data){
+                    console.log(data[key].acf.nome)
+                    let htmlSegment = `
+                    <div class="card swiper-slide">
+                    <div class="card-inner">
+                            <div class="card-front">
+                                <img src="${data[key].acf.foto}" alt="${data[key].slug}">
+                            </div>
+                            <div class="card-back">
+                                <div class="card__content">
+                                    <h3>${data[key].acf.nome}</h3>
+                                    <p class="subtitulo">${data[key].acf.funcao}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+                    auxiliar+=htmlSegment
+                }
+                let swiper = document.querySelector(tipo_de_funcionario);
+                swiper.innerHTML = auxiliar;
+            }
+            ))
+        )
+}
+get_funcionarios('#prof1', 'https://dominium-api.nordevtech.com.br/wp-json/dominium/equipe/professores-i')
